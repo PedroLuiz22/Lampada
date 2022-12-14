419a44ac287c53af0bab33d506ca69fadc6d077c
Pedro Luiz <pedrolluiz@hotmail.com>
2022-12-14 01:34:15 -0300

    Projeto finalizado com adição do JS e incremeto CSS

---
+++ lampada.js
@@ -0,0 +1,16 @@
+var lamp = document.getElementById('lampada')
+function quebrou(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+function ligar(){
+    if(!quebrou())
+    lamp.src = 'ligada.svg'
+}
+function desligar(){
+    if(!quebrou())
+    lamp.src = "./desligada.svg"
+}
+lamp.addEventListener ('click', quebrar)
+function quebrar(){
+    lamp.src = "./quebrada.svg"
+}
